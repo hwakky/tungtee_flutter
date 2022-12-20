89bf0fad4ef190ea3b5cbfd709fcc51fe1753928
big555ddd <biggafu5@hotmail.com>
2022-12-20 17:17:01 +0700

    firebase = true

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,21 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:tangteevs/firebase_options.dart';
 import 'Login.dart';
 import 'Register.dart';
 import 'HomePage.dart';
 
-
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MaterialApp(
     initialRoute: '/login',
     routes: {
       '/login': (context) => Login(),
-    
       '/register': (context) => RegistrationScreen(),
       '/HomePage': (context) => MyHomePage(),
-
     },
   ));
 }
---
+++ lib/services/Firebase Authentication.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import '/utils/showSnackbar.dart';
+
+class FirebaseAuthMethods {
+  final FirebaseAuth _auth;
+  FirebaseAuthMethods(this._auth);
+
+  // FOR EVERY FUNCTION HERE
+  // POP THE ROUTE USING: Navigator.of(context).pushNamedAndRemoveUntil('/', (Route<dynamic> route) => false);
+
+  // GET USER DATA
+  // using null check operator since this method should be called only
+  // when the user is logged in
+  User get user => _auth.currentUser!;
+
+  // STATE PERSISTENCE STREAM
+  Stream<User?> get authState => FirebaseAuth.instance.authStateChanges();
+  // OTHER WAYS (depends on use case):
+  // Stream get authState => FirebaseAuth.instance.userChanges();
+  // Stream get authState => FirebaseAuth.instance.idTokenChanges();
+  // KNOW MORE ABOUT THEM HERE: https://firebase.flutter.dev/docs/auth/start#auth-state
+
+  // EMAIL SIGN UP
+  Future<void> signUpWithEmail({
+    required String email,
+    required String password,
+    required BuildContext context,
+  }) async {
+    try {
+      await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      await sendEmailVerification(context);
+    } on FirebaseAuthException catch (e) {
+      // if you want to display your own custom error message
+      if (e.code == 'weak-password') {
+        print('The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        print('The account already exists for that email.');
+      }
+      showSnackBar(
+          context, e.message!); // Displaying the usual firebase error message
+    }
+  }
+
+  // EMAIL LOGIN
+  Future<void> loginWithEmail({
+    required String email,
+    required String password,
+    required BuildContext context,
+  }) async {
+    try {
+      await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      if (!user.emailVerified) {
+        await sendEmailVerification(context);
+        // restrict access to certain things using provider
+        // transition to another page instead of home screen
+      }
+    } on FirebaseAuthException catch (e) {
+      showSnackBar(context, e.message!); // Displaying the error message
+    }
+  }
+
+  // EMAIL VERIFICATION
+  Future<void> sendEmailVerification(BuildContext context) async {
+    try {
+      _auth.currentUser!.sendEmailVerification();
+      showSnackBar(context, 'Email verification sent!');
+    } on FirebaseAuthException catch (e) {
+      showSnackBar(context, e.message!); // Display error message
+    }
+  }
+
+  // SIGN OUT
+  Future<void> signOut(BuildContext context) async {
+    try {
+      await _auth.signOut();
+    } on FirebaseAuthException catch (e) {
+      showSnackBar(context, e.message!); // Displaying the error message
+    }
+  }
+
+}
